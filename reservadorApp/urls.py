5e6c91070f6dd23f39a1a55bb4b56936d214642f
simon <simon.marambio@inacapmail.cl>
2025-11-20 14:17:40 -0300

    quinto commit

--- reservadorApp/urls.py
+++ reservadorApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path("sala/<int:id>", views.detalle_sala, name="detalle_sala"),
+    path("lista_salas/", views.SalaListView.as_view(), name="lista_salas"),
     path("reservar/", views.crear_reserva, name="crear_reserva"),
 ]
--- reservadorApp/views.py
+++ reservadorApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
 from django.utils import timezone
 from .models import Sala, Reserva
 from .forms import ReservaForm
@@ -14,18 +15,27 @@ def home(request):
 
 def detalle_sala(request, id):
     sala = get_object_or_404(Sala, id=id)
-
     reserva = Reserva.objects.filter(sala=sala).order_by('-id').first()
+    context = {
+        "sala" : sala,
+        "reserva" : reserva,
+    }
 
     #para liberar salas
     if reserva and reserva.fecha_termino < timezone.now():
         sala.disponible = True
         sala.save()
 
-    return render(request, 'detalle_sala.html', {
-        "sala": sala,
-        "reserva": reserva,
-    })
+    return render(request, 'detalle_sala.html', context)
+
+class SalaListView(generic.ListView):
+    model = Sala
+    template_name = "lista_salas.html"
+    context_object_name = "salas"
+
+    #Trae la lista, y la ordena por nombre
+    def get_queryset(self):
+        return Sala.objects.all().order_by('nombre')
 
 def crear_reserva(request):
     if request.method == "POST":
@@ -39,4 +49,8 @@ def crear_reserva(request):
     else:
         form = ReservaForm()
 
-    return render(request, "crear_reserva.html", {'form': form})
+    context = {
+        "form":form,
+        }
+
+    return render(request, "crear_reserva.html", context)
